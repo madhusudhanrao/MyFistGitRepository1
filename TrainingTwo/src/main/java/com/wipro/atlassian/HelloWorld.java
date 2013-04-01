e6a8c4bd60c5b87de06178a7d4a5ab0724a53ce7
MadhuSudhanRaoV <madhu09v@gmail.com>
2013-04-01 12:54:57 +0530

    modified the java code

--- TrainingTwo/src/main/java/com/wipro/atlassian/HelloWorld.java
+++ TrainingTwo/src/main/java/com/wipro/atlassian/HelloWorld.java
@@ -10,6 +10,7 @@ package com.wipro.atlassian;
 public class HelloWorld {
 	public static void main(String args[]){
 	System.out.println("Hello India....");
+	System.out.println("Hello India.aaaaaaaaaaaaaaaaaa...");
 	}
 
 }
