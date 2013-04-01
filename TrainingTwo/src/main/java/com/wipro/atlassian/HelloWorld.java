1d9b0201e79b119f5125aef74114b7811c1731b5
MadhuSudhanRaoV <madhu09v@gmail.com>
2013-04-01 11:16:24 +0530

    added new project for training

---
+++ TrainingTwo/src/main/java/com/wipro/atlassian/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.wipro.atlassian;
+
+/**
+ * @author user
+ *
+ */
+public class HelloWorld {
+	public static void main(String args[]){
+	System.out.println("Hello India....");
+	}
+
+}
---
+++ TrainingTwo/src/test/java/AppTest.java
@@ -0,0 +1,23 @@
+import org.junit.After;
+import org.junit.Before;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+	
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+	
+	public void test() {
+		//fail ("Not yet implemented")
+	}
+}
